b16fbb04b2ff95f952e4d4876e31e0da647c2826
Diarmuid Ryan <diarmuidr3d@gmail.com>
2015-08-24 14:26:31 +0100

    Removed useless code in drawUprightBox in graphics.js

--- js/graphics.js
+++ js/graphics.js
@@ -469,11 +469,6 @@ function drawUprightBox(coords, material) {
     geometry.faces.push(new THREE.Face3(2, 1, 0));
     geometry.faces.push(new THREE.Face3(3, 2, 0));
     var mesh = new THREE.Mesh(geometry, material);
-    mesh.geometry.verticesNeedUpdate = true;
-    mesh.geometry.normalsNeedUpdate = true;
-    mesh.geometry.computeFaceNormals();
-    mesh.geometry.computeVertexNormals();
-    mesh.geometry.computeBoundingSphere();
     return mesh;
 }
 
